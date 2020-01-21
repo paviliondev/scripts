cf93a383831c8316502f9b26f4fedc48bcfa55d5
Angus McLeod <angus@mcleod.org.au>
2020-01-21 16:35:20 +1100

    Add real scripts

--- remove_non_core_plugins.sh
+++ remove_non_core_plugins.sh
@@ -1,1 +1,27 @@
-/Users/angusmcleod/civically/discourse/plugins/civically-scripts/remove_non_core_plugins.sh
+#!/usr/bin/env bash
+
+## This script assumes:
+#
+# The folder assigned to discourseRoot contains:
+# - The discourse repo in folder called 'discourse'
+##
+
+discourseRoot=~/discourse
+
+declare -a corePlugins=(
+  "discourse-details"
+  "discourse-internet-explorer"
+  "discourse-local-dates"
+  "discourse-narrative-bot"
+  "discourse-presence"
+  "lazy-yt"
+  "poll"
+)
+
+for d in $discourseRoot/discourse/plugins/*/; do
+  name="$(basename $d)"
+
+  if [[ ! "${corePlugins[@]}" =~ "${name}" ]]; then
+    unlink ${d%/}
+  fi
+done
--- setup_discourse_plugin.sh
+++ setup_discourse_plugin.sh
@@ -1,1 +1,38 @@
-/Users/angusmcleod/civically/discourse/plugins/civically-scripts/setup_discourse_plugin.sh
+#!/usr/bin/env bash
+
+## Setup a Discourse plugin(s).
+
+## Usage
+# <path-to-script>/setup_discourse_plugin.sh <plugin-name> <plugin-name> ...
+##
+
+## This script assumes
+# The following scripts are in the same directory and are executable:
+# - remove_non_core_plugins.sh
+#
+# The folder assigned to discourseRoot contains:
+# - The discourse repo in folder called 'discourse'
+# - A folder called 'plugins' containing the plugin to be setup
+#
+# See included scripts for further assumptions
+##
+
+scriptRoot=${0%/*}
+discourseRoot=~/discourse
+
+# Check if at least one plugin name has been passed
+if [ $# -eq 0 ]; then
+  echo "Usage: ./setup_discourse_plugin plugin_name"
+  exit 1
+fi
+
+## First, remove current non-core plugins.
+
+$scriptRoot/remove_non_core_plugins.sh
+
+## Then add the plugin(s).
+
+for plugin in "$@"
+do
+  ln -s $discourseRoot/plugins/$plugin $discourseRoot/discourse/plugins/$plugin
+done
